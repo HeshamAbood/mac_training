1eba0f8f164ec618ef8d7a37c56b1f360d4f2d54
HeshamAbood <hshm_aboud@yahoo.com>
2018-09-18 18:27:03 +0300

    add __give_cash

--- atm_refactored.py
+++ atm_refactored.py
@@ -1,15 +1,13 @@
-import collections
+from collections import OrderedDict
 
 class ATM():
-    __atm_banknotes = collections.OrderedDict(
+    __atm_banknotes = OrderedDict(
         {"100": 0, "50": 0, "20": 0, "10": 0, "5": 0, "2": 0, "1": 0})
     __pw = "123456"
 
-    def __init__(self, user_balance=0, atm_balance=collections.OrderedDict(
+    def __init__(self, user_balance=0, atm_balance=OrderedDict(
         {"100": 50, "50": 50, "20": 50, "10": 50, "5": 50, "2": 50, "1": 50})):
-
        ATM.__atm_banknotes=atm_balance
-
        self.user_balance= user_balance
 
     @classmethod
@@ -19,12 +17,10 @@ class ATM():
 
     @classmethod
     def calc_banknotes(cls, amount):
-        result = collections.OrderedDict({"100": 0, "50": 0, "20": 0, "10": 0, "5": 0, "2": 0, "1": 0})
-
+        result = OrderedDict({"100": 0, "50": 0, "20": 0, "10": 0, "5": 0, "2": 0, "1": 0})
         for name in result.keys():
             result[name] = amount // int(name)
             amount = amount % int(name)
-
         return result
 
     def withdraw(self, request):
@@ -34,18 +30,19 @@ class ATM():
             print("Can't give you all this money !!")
             return
 
-        user_withdraw_banknotes = self.calc_banknotes(request)
+        request_banknotes = self.calc_banknotes(request)
 
-        for name, val in user_withdraw_banknotes.items():
+        for name, val in request_banknotes.items():
             ATM.__atm_banknotes[name] = ATM.__atm_banknotes[name] - val
 
         self.user_balance=self.user_balance-request
-
-        user_withdraw=[( name, val) for name,val in user_withdraw_banknotes.items() if val>0]
-        for ( name, val) in user_withdraw:
+        self.__give_cash(request_banknotes)
+        
+    def __give_cash(self, banknotes_to_withdraw):
+        user_withdraw_banknotes=[( name, val) for name,val in banknotes_to_withdraw.items() if val>0]
+        for ( name, val) in user_withdraw_banknotes:
             for _ in range(val):
                 print ("Give "+ str(name))
-
 
     @classmethod
     def get_atm_available_money(cls, pw):
@@ -54,7 +51,6 @@ class ATM():
             print("Available money papers in the ATM: " + str(ATM.__atm_banknotes))
         else:
             print("You are not authorize for this operation")
-
 
 #ATM().get_atm_available_money("123456")
 balance=500
